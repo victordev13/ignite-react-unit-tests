ba4cfe879da24b3c1b96eb2f242683d1e433136f
victordev13 <vtrcarvalho.13@gmail.com>
2022-03-05 14:55:02 -0300

    'fix: use jest.mocked instead "mocked"

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,13 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { useSession } from 'next-auth/client';
-import { mocked } from 'ts-jest/utils';
 import { SignInButton } from '.';
 
 jest.mock('next-auth/client');
 
 describe('SignInButton component', () => {
   it('renders correctly when user is not authenticated', () => {
-    const useSessionMocked = mocked(useSession);
+    const useSessionMocked = jest.mocked(useSession);
     useSessionMocked.mockReturnValueOnce([null, false]);
 
     render(<SignInButton />);
@@ -18,7 +17,7 @@ describe('SignInButton component', () => {
   });
 
   it('renders correctly when user is authenticated', () => {
-    const useSessionMocked = mocked(useSession);
+    const useSessionMocked = jest.mocked(useSession);
     useSessionMocked.mockReturnValueOnce([
       {
         user: {
