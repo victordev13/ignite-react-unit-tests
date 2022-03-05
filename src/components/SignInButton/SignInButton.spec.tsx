5ccc2b5b03b9cb24b4f23c7cca1408caedaf3947
victordev13 <vtrcarvalho.13@gmail.com>
2022-03-05 13:56:35 -0300

    feat: test SignInButton, using "mocked"(ts-jest/utils) function to different returns

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import { useSession } from 'next-auth/client';
+import { mocked } from 'ts-jest/utils';
+import { SignInButton } from '.';
+
+jest.mock('next-auth/client');
+
+describe('Header component', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SignInButton />);
+
+    expect(
+      screen.getByText('Sign in with GitHub')
+    ).toBeInTheDocument();
+  });
+
+  it('renders correctly when user is authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'John Doe',
+          email: 'johndoe@gmail.com',
+        },
+      },
+      true,
+    ]);
+
+    render(<SignInButton />);
+
+    expect(
+      screen.getByText('John Doe')
+    ).toBeInTheDocument();
+  });
+});
